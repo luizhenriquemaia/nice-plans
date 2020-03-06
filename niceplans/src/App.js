5497e2ad0ebf8c42a181185c6db37dc0f8c49799
LuizHenriqueMaia <luizhenriquembh@gmail.com>
2020-03-06 20:35:29 -0300

    Initial setting up

---
+++ niceplans/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom'
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <h1>Nice Plans</h1>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
