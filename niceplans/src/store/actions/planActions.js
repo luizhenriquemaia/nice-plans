04889ee8278eb7a1331c84e66cdbe269b6b0621d
LuizHenriqueMaia <luizhenriquembh@gmail.com>
2020-03-14 18:58:43 -0300

    Adding data to firebase

--- niceplans/src/store/actions/planActions.js
+++ niceplans/src/store/actions/planActions.js
@@ -1,6 +1,17 @@
 export const createPlan = (plan) => {
-    return (dispatch, getState) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
         // make async call to database
-        dispatch({ type: 'CREATE_PLAN', plan: plan })
+        const firestore = getFirestore()
+        firestore.collection('plans').add({
+            ...plan,
+            authorFirstName: 'Luiz',
+            authorLastName: 'Henrique',
+            authorId: 12345,
+            createdAt: new Date()
+        }).then(() => {
+            dispatch({ type: 'CREATE_PLAN', plan: plan })
+        }).catch((err) => {
+            dispatch({ type: 'CREATE_PLAN_ERROR', err: err })
+        })
     }
 }
