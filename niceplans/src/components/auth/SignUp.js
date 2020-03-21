2edcdbfac60fe671573d1631e2f7e59bbb0ad354
LuizHenriqueMaia <luizhenriquembh@gmail.com>
2020-03-21 10:11:19 -0300

    signup ok

--- niceplans/src/components/auth/SignUp.js
+++ niceplans/src/components/auth/SignUp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { Redirect } from 'react-router-dom'
+import { signUp } from '../../store/actions/authActions'
 
 
 class SignUp extends Component {
@@ -17,10 +18,10 @@ class SignUp extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault()
-        console.log(this.state)
+        this.props.signUp(this.state)
     }
     render() {
-        const { auth } = this.props
+        const { auth, authError } = this.props
         if (auth.uid) return <Redirect to='/' />
         return (
             <div className="container">
@@ -45,6 +46,9 @@ class SignUp extends Component {
                     <div className="input-field grid">
                         <button className="sign-btn">Sign Up</button>
                     </div>
+                    <div>
+                        { authError ? <p>{authError}</p> : null }
+                    </div>
                 </form>
             </div>
         )
@@ -53,9 +57,16 @@ class SignUp extends Component {
 
 const mapStateToProps = (state) => {
     return {
-        auth: state.firebase.auth
+        auth: state.firebase.auth,
+        authError: state.auth.authError
+    }
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        signUp: (newUser) => dispatch(signUp(newUser))
     }
 }
 
 
-export default connect(mapStateToProps)(SignUp)
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp)
